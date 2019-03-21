1481004f5306f0463d91c26f5b07f69cff3fa1a6
EvaSpessotto <eva@spessotto.com>
2019-03-21 13:50:45 +0100

    Add gatsby-browser and theming is working :tada:

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Navigation from './global/Navigation'
 import Footer from './global/Footer'
+import "semantic-ui-less/semantic.less";
 
 const Layout = ({ children }) => {
   return (
